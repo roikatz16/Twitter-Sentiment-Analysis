9486c64e84114e8c8586ba325b23016474c23873
Michal Talmor <talmormi@post.bgu.ac.il>
2021-01-05 23:36:26 +0200

    fix

--- TwiterSemanticModel.py
+++ TwiterSemanticModel.py
@@ -31,6 +31,58 @@ class TwitterSemanticModel():
             del dataset[col]
         return dataset
 
+    def get_hashtags(self, tweet):
+        hashtag = re.findall(r'\#\w+', tweet)
+        if len(hashtag) == 0:
+            return 0
+        return 1
+
+    def get_tags(self, tweet):
+        tag = re.findall(r'\@\w+', tweet)
+        if len(tag) == 0:
+            return 0
+        return 1
+
+    def get_tweet_length(self, tweet):
+        return len(tweet)
+
+    def count_question_marks(self, tweet):
+        tweet = re.sub(r"http\S+|www\S+|https\S+", '', tweet, flags=re.MULTILINE)
+        return tweet.count('?')
+
+    def count_exclamation_marks(self, tweet):
+        tweet = re.sub(r"http\S+|www\S+|https\S+", '', tweet, flags=re.MULTILINE)
+        return tweet.count('!')
+
+    def count_upper(self, tweet):
+        tweet = re.sub(r"http\S+|www\S+|https\S+", '', tweet, flags=re.MULTILINE)
+        # Remove user @ references and '#' from tweet
+        tweet = re.sub(r'\@\w+|\#', '', tweet)
+        # Remove punctuations
+        tweet = tweet.translate(str.maketrans('', '', string.punctuation))
+        return sum(map(str.isupper, tweet.split()))
+
+    def sad_smilies(self, tweet):
+        tweet = re.sub(r"http\S+|www\S+|https\S+", '', tweet, flags=re.MULTILINE)
+        tweet = re.sub(r'\@\w+|\#', '', tweet)
+        sad_faces = '>:[ :-( :( :-c :c :-< :< :-[ :[ :{ :| |: :/ /: 8( 8-( ]:< )-: ): >-: >: ]-: ]: }: )8 )-8'.split(' ')
+        for face in sad_faces:
+            if face in tweet:
+                return 1
+        return 0
+
+
+    def happy_smilies(self, tweet):
+        tweet = re.sub(r"http\S+|www\S+|https\S+", '', tweet, flags=re.MULTILINE)
+        tweet = re.sub(r'\@\w+|\#', '', tweet)
+        happy_faces = ':-) :) :D :o) :] :3 :c) :> =] 8) =) :} :^) :-D 8-D 8D x-D xD X-D XD =-D =D =-3 =3 B^D (-: (: (' \
+                      'o: [: <: [= (8 (= {: (^:'.split(' ')
+        for face in happy_faces:
+            if face in tweet:
+                return 1
+        return 0
+
+
     def preprocess_tweet_text(self, tweet):
         stop_words = set(stopwords.words('english'))
         tweet.lower()
@@ -105,17 +157,17 @@ class TwitterSemanticModel():
     def cross_validation(self, X_train, Y_train, scoring):
         models = []
         models.append(("LR", LogisticRegression(solver='lbfgs')))
-        # models.append(("KNN", KNeighborsClassifier()))
+        models.append(("KNN", KNeighborsClassifier()))
         # models.append(("NB", GaussianNB()))
-        # models.append(("NB", MultinomialNB()))
-        # models.append(("SVM", svm.SVC()))
+        models.append(("NB", MultinomialNB()))
+        models.append(("SVM", svm.SVC()))
         results = []
         names = []
         for name, model in models:
             cv_results = cross_val_score(model, X_train, Y_train, cv=10, scoring=scoring)
             results.append(cv_results)
             names.append(name)
-            msg = f"{name}, {cv_results.mean()}, {cv_results.std()}"
+            msg = f"{name} {scoring}, mean:{cv_results.mean()}, std:{cv_results.std()}"
             print(msg)
         return results, models
 
@@ -125,11 +177,9 @@ class TwitterSemanticModel():
         return y_predict
 
     def combine_features(self, X, other_features):
-        # other_feature shoul be a list
-        combined_features = X.copy()
-        for feature in other_features:
-            other_feature = csr_matrix(feature).transpose()
-            combined_features = hstack(combined_features, other_feature)
+        # other_feature should be a list
+        csr_features = csr_matrix(other_features).transpose()
+        combined_features = hstack((X, csr_features))
         return combined_features
 
 
@@ -138,7 +188,20 @@ if __name__ == '__main__':
     TSM = TwitterSemanticModel()
     # Load dataset
     dataset = TSM.load_dataset("Train.csv")
+    # dataset = dataset.iloc[:200]
+
     # Preprocess data
+    hashtags = (dataset['SentimentText'].apply(TSM.get_hashtags)).tolist()
+    tags = (dataset['SentimentText'].apply(TSM.get_tags)).tolist()
+    tweet_length = (dataset['SentimentText'].apply(TSM.get_tweet_length)).tolist()
+
+    feature_question_marks = dataset['SentimentText'].apply(TSM.count_question_marks).to_list()
+    feature_exclamation_marks = dataset['SentimentText'].apply(TSM.count_exclamation_marks).to_list()
+    feature_upper_words = dataset['SentimentText'].apply(TSM.count_upper).to_list()
+
+    feature_sad = dataset['SentimentText'].apply(TSM.sad_smilies).to_list()
+    feature_happy = dataset['SentimentText'].apply(TSM.happy_smilies).to_list()
+
     dataset.SentimentText = dataset['SentimentText'].apply(TSM.preprocess_tweet_text)
     # Same tf vector will be used for Testing sentiments on unseen trending data
     tf_vector = TSM.get_feature_vector(np.array(dataset.iloc[:, 1]).ravel())
@@ -146,15 +209,28 @@ if __name__ == '__main__':
 
     other_features = []
     # TODO: add to combine_features more features
-    # X = TSM.combine_features(X, other_features)
+    # other_features.append(hashtags)
+    # other_features.append(tags)
+    # other_features.append(tweet_length)
+
+    # other_features.append(feature_question_marks)
+    # other_features.append(feature_exclamation_marks)
+    # other_features.append(feature_upper_words)
+
+    other_features.append(feature_sad)
+    other_features.append(feature_happy)
+
+    X = TSM.combine_features(X, other_features)
 
     y = np.array(dataset.iloc[:, 0]).ravel()
 
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=30)
     ##########CROSS VALIDATION################
     # cross_val_results, models = TSM.cross_validation(X_train, y_train, scoring='accuracy')
+    # cross_val_results, models = TSM.cross_validation(X_train, y_train, scoring='precision')
+    # cross_val_results, models = TSM.cross_validation(X_train, y_train, scoring='recall')
     #########TRAINING MODEL####################
-    model = LogisticRegression(solver='lbfgs')  # choose model
+    model = svm.SVC()  # choose model
     y_predict = TSM.fit_predict_model(model, X_train, y_train, X_test)
     ########GET ACCURACY SCORE################
     print(accuracy_score(y_test, y_predict))
@@ -164,12 +240,34 @@ if __name__ == '__main__':
     test_file_name = "Test.csv"
     test_ds = TSM.load_dataset(test_file_name)
     # Preprocess data
+    hashtags = (test_ds['SentimentText'].apply(TSM.get_hashtags)).tolist()
+    tags = (test_ds['SentimentText'].apply(TSM.get_tags)).tolist()
+    tweet_length = (test_ds['SentimentText'].apply(TSM.get_tweet_length)).tolist()
+
+    feature_question_marks = test_ds['SentimentText'].apply(TSM.count_question_marks).to_list()
+    feature_exclamation_marks = test_ds['SentimentText'].apply(TSM.count_exclamation_marks).to_list()
+    feature_upper_words = test_ds['SentimentText'].apply(TSM.count_upper).to_list()
+
+    feature_sad = test_ds['SentimentText'].apply(TSM.sad_smilies).to_list()
+    feature_happy = test_ds['SentimentText'].apply(TSM.happy_smilies).to_list()
+
     test_ds.SentimentText = test_ds['SentimentText'].apply(TSM.preprocess_tweet_text)
 
     X = TSM.get_tweet_features_by_TfidfVectorizer(test_ds, tf_vector)
     other_features = []
     # TODO: add to combine_features more features
-    # X = TSM.combine_features(X, other_features)
+    # other_features.append(hashtags)
+    # other_features.append(tags)
+    # other_features.append(tweet_length)
+
+    # other_features.append(feature_question_marks)
+    # other_features.append(feature_exclamation_marks)
+    # other_features.append(feature_upper_words)
+
+    other_features.append(feature_sad)
+    other_features.append(feature_happy)
+
+    X = TSM.combine_features(X, other_features)
 
     y_predict = model.predict(X)
     # save results
